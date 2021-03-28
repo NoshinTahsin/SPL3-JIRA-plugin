56cef4062e538903dbb413ffc2198993d533de44
Noshin Tahsin <noshintahsiniitdu@gmail.com>
2021-03-28 08:01:38 -0700

    row clickable

--- public/js/addon.js
+++ public/js/addon.js
@@ -61,27 +61,59 @@ function formChanged(){
       tempImage = "<img src='https://cdn.pixabay.com/photo/2016/08/08/09/17/avatar-1577909_960_720.png' width='24'>";
 
       var AssigneeTable = document.getElementById("assigneeTable");
-          for(i=0;i<5;i++)
+      
+        /*for(i=0;i<5;i++)
           {
             var newRow = AssigneeTable.insertRow(-1);
             var newCellAssignee = newRow.insertCell(0);
-            var newCellAssigneeID = newRow.insertCell(1);
-            var newCellAssigneeName = newRow.insertCell(2);
+            //var newCellAssigneeID = newRow.insertCell(1);
+            var newCellAssigneeName = newRow.insertCell(1);
             //story icon
             newCellAssignee.innerHTML = tempImage;
-            newCellAssigneeID.innerHTML=a_list[0][i+5];
+           // newCellAssigneeID.innerHTML=a_list[0][i+5];
             newCellAssigneeName.innerHTML=a_list[0][i];
     
             //AssigneeTable.deleteRow(1);
-         }
+         }*/
+
+         var av1 = document.getElementById("avatar_r1");
+         var av2 = document.getElementById("avatar_r2");
+         var av3 = document.getElementById("avatar_r3");
+         var av4 = document.getElementById("avatar_r4");
+         var av5 = document.getElementById("avatar_r5");
+
+         var nm1 = document.getElementById("name_r1");
+         var nm2 = document.getElementById("name_r2");
+         var nm3 = document.getElementById("name_r3");
+         var nm4 = document.getElementById("name_r4");
+         var nm5 = document.getElementById("name_r5");
+
+         av1.innerHTML = tempImage;
+         av2.innerHTML = tempImage;
+         av3.innerHTML = tempImage;
+         av4.innerHTML = tempImage;
+         av5.innerHTML = tempImage;
+         
+         nm1.innerHTML = a_list[0][0];
+         nm2.innerHTML = a_list[0][1];
+         nm3.innerHTML = a_list[0][2];
+         nm4.innerHTML = a_list[0][3];
+         nm5.innerHTML = a_list[0][4];
+
+
       }
-  });
+  });  
+
 
   
 }
 
 function showIssue(){
 
+}
+
+function rowClicked(){
+  alert("row clicked");
 }
 
 
